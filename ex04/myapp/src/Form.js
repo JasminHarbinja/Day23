1e9f09808fcb41bc2c3377b8d65f2333ec43e215
Jasmin Harbinja <57997257+JasminHarbinja@users.noreply.github.com>
2021-03-31 00:34:36 +0200

    Add files via upload

---
+++ ex04/myapp/src/Form.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Form extends Component {
+    initialState = {
+        firstName: "",
+        lastName: "",
+    };
+    state = this.initialState;
+}
+export default Form;
